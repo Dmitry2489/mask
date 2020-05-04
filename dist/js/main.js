04f1a121b79b7e8e683768e69e9cdd5b2d87355d
Dmitry2489 <iva.klicunov2489@mail.ru>
2020-05-04 21:37:03 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+
+
+  
+});
+
+let progress = document.getElementById('progressbar'),
+    totalHeight = document.body.scrollHeight - window.innerHeight;
+
+    window.addEventListener('scroll',  function () {
+        console.log('scroll')
+        let progressHight = (window.pageYOffset / totalHeight) * 100;
+        progress.style.height = progressHight + '%';
+    });
